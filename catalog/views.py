00a873bbafe1101dabcf3cab019158925b58b53b
Nick <nik.linnik.001@gmail.com>
2021-09-19 21:53:31 +0300

    Add main page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Author, Book, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.all().filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_books_with_crime_word = Book.objects.all().filter(title__contains='Crime').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_crime_word': num_books_with_crime_word,
+    }
+
+    return render(request, 'index.html', context)
